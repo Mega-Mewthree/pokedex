1dbd10bb94a0bfd814c70662b74667ff7972cb8a
Mega_Mewthree <Mega-Mewthree@users.noreply.github.com>
2018-12-02 12:45:15 -0800

    New JSON file

--- scripts/parse-pokemon-data.js
+++ scripts/parse-pokemon-data.js
@@ -8,15 +8,26 @@ csv().fromFile(pathToCsv).subscribe((resultRow) => {
     var name = resultRow.identifier;
     delete resultRow.identifier;
     resultRow.name = name;
-    if (resultRow.species_id < 650) {
-        resultRow.sprites = {
-            normal: 'http://img.pokemondb.net/sprites/black-white/normal/' + name + '.png',
-            animated: 'http://img.pokemondb.net/sprites/black-white/anim/normal/' + name + '.gif'
-        };
-    } else {
+    if (resultRow.species_id === 808) {
+      resultRow.sprites = {
+        normal: 'https://img.pokemondb.net/sprites/sun-moon/icon/meltan.png'
+      };
+    } else if (resultRow.species_id === 809) {
+      resultRow.sprites = {
+        normal: 'https://img.pokemondb.net/sprites/sun-moon/icon/melmetal.png'
+      };
+    } else if (resultRow.species_id < 722) {
         resultRow.sprites = {
             normal: 'http://img.pokemondb.net/sprites/x-y/normal/' + name + '.png'
         };
+    } else if (resultRow.species_id < 803) {
+        resultRow.sprites = {
+            normal: 'http://img.pokemondb.net/sprites/sun-moon/normal/' + name + '.png'
+        };
+    } else {
+      resultRow.sprites = {
+          normal: 'http://img.pokemondb.net/sprites/ultra-sun-ultra-moon/normal/' + name + '.png'
+      };
     }
 })
 .then(obj => {
